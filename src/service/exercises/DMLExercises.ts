64de03fdd0cd4f98a30652e043134724ffe0d3dc
Tyson Thomas <tyson.thomas8@tafensw.edu.au>
2025-12-18 19:50:15 +1100

    Added another DML exercise

--- src/service/exercises/DMLExercises.ts
+++ src/service/exercises/DMLExercises.ts
@@ -49,7 +49,7 @@ export const Exercises: ExercisePack = {
         .expectedData(DMLExercises.ex2data)
     .done(),
     NewTask()
-      .name("Update Products - 3")
+      .name("Update Products - 4")
       .key('dml03')
       .scaffold("-- Your Query Below --")
       .question(DMLExercises.ex3)
@@ -71,6 +71,30 @@ export const Exercises: ExercisePack = {
         .dataManipulationEval()
         .selectStatement("SELECT product_id, name, price FROM Product;")
         .expectedData(DMLExercises.ex3data)
+    .done(),
+    NewTask()
+      .name("Delete Products - 4")
+      .key('dml04')
+      .scaffold("-- Your Query Below --")
+      .question(DMLExercises.ex4)
+      .database('delete04')
+      .setup()
+        .add({
+            command: SqliteCommands.Exec,
+            operation: PointOfSaleDB.getSchema(),
+            extra: []
+          })
+        .add({
+            command: SqliteCommands.Exec,
+            operation: PointOfSaleDB.getInsertData(),
+            extra: []
+          })
+        .skip()
+      .evaluation("DML")
+        .test('delete-products-1')
+        .dataManipulationEval()
+        .selectStatement("SELECT product_id, name, price FROM Product;")
+        .expectedData(DMLExercises.ex4data)
     .done(),
   ]
 };
--- src/service/exercises/dml/Exported.ts
+++ src/service/exercises/dml/Exported.ts
@@ -7,8 +7,8 @@ import { markdown as ex2 } from './ex_02.md';
 //@ts-ignore
 import { markdown as ex3 } from './ex_03.md';
 
-// //@ts-ignore
-// import { markdown as ex4 } from './ex_04.md';
+//@ts-ignore
+import { markdown as ex4 } from './ex_04.md';
 // //@ts-ignore
 // import { markdown as ex5 } from './ex_05.md';
 // //@ts-ignore
@@ -21,6 +21,7 @@ import { markdown as ex3 } from './ex_03.md';
 import ex1data from './ex_01_data.json';
 import ex2data from './ex_02_data.json';
 import ex3data from './ex_03_data.json';
+import ex4data from './ex_04_data.json';
 
 
 // Exercises Batch
@@ -28,7 +29,7 @@ export const DMLExercises = {
   ex1, ex1data,
   ex2, ex2data,
   ex3, ex3data,
-  // ex4, ex4json,
+  ex4, ex4data,
   // ex5, ex5json,
   // ex6, ex6json,
   // ex7, ex7json,
